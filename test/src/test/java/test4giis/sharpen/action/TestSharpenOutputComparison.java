a86b01c7cd24de7dee2d4b782dee92c9589a9c88
Javier <tuya@uniovi.es>
2023-06-18 08:00:21 +0200

    Review action.yml and test

--- test/src/test/java/test4giis/sharpen/action/TestSharpenOutputComparison.java
+++ test/src/test/java/test4giis/sharpen/action/TestSharpenOutputComparison.java
@@ -1,11 +1,11 @@
 package test4giis.sharpen.action;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import giis.visualassert.VisualAssert;
 
 /**
  * Check that translation works by comparing translated files (in src/main/main.net) and the expected (in expected)
@@ -15,12 +15,12 @@ public class TestSharpenOutputComparison {
 	public void testTranslatedMainApp() throws Throwable {
 		String expected=FileUtils.readFileToString(new File("expected/MainApp.cs"), "UTF-8").replace("\r","");
 		String actual=FileUtils.readFileToString(new File("src/main/main.net/Giis.Sharpen.Action/MainApp.cs"), "UTF-8").replace("\r","");
-		assertEquals(expected, actual);
+		new VisualAssert().setBrightColors(true).assertEquals(expected, actual);
 	}
 	@Test
 	public void testTranslatedSimpleClass() throws Throwable {
 		String expected=FileUtils.readFileToString(new File("expected/SimpleClass.cs"), "UTF-8").replace("\r","");
 		String actual=FileUtils.readFileToString(new File("src/main/main.net/Giis.Sharpen.Action/SimpleClass.cs"), "UTF-8").replace("\r","");
-		assertEquals(expected, actual);
+		new VisualAssert().setBrightColors(true).assertEquals(expected, actual);
 	}
 }
